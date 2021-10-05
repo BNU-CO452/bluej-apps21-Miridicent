19c1eef2fed3ffcbcb4cb8c5c47469fef665b0de
Conor Briggs <conorhbriggs@gmail.com>
2021-10-05 11:29:30 +0100

    Create Module.java

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    // Unique Module code 
+    private String code;
+    
+    // Title of the module 
+    private String title;
+    
+    //Credit Value for the module
+    private int credit;
+
+    /**
+     * Create a module with code, title and credit 
+     */
+    public Module(String code, String title, int credit)
+    {
+        // initialise instance variables
+        this.code = code;
+        this.title = title;
+        this.credit = credit; 
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void printheading()
+    {
+        // put your code here
+        System.out.println(" -------------------------- ");
+        
+    }
+    
+    
+
