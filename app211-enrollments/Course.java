c1020b9dab3e18792e5bb7481b6dca6b7e06ff4d
Conor Briggs <conorhbriggs@gmail.com>
2021-10-12 10:53:00 +0100

    Completed App

--- app211-enrollments/Course.java
+++ app211-enrollments/Course.java
@@ -36,7 +36,7 @@ public class Course
         printHeading();
         
         System.out.println(" Course Code: " + code + ": " + title);
-        System.out.println();
+        module.print();
     }
     
     /**
