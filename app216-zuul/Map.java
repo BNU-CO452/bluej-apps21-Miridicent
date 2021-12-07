497f07604d2b64c95246f0464e1f69af90179f8a
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2021-12-07 11:46:43 +0000

    added locations

--- app216-zuul/Map.java
+++ app216-zuul/Map.java
@@ -16,9 +16,11 @@ public class Map
 {
     // Need to add a list of exits
     
-    private Location outside, theater, pub, lab, office;
+    private Location entrance, chamber, hallwayone, hallwaytwo, study, magiclab, kitchen, dining, stairs, centre;
 
     private Location currentLocation;
+    
+    private int dispell;
 
     /**
      * Constructor for objects of class Map
@@ -26,6 +28,7 @@ public class Map
     public Map()
     {
         createLocations();
+        dispell = 0;
     }
 
     /**
@@ -36,69 +39,135 @@ public class Map
      */
     private void createLocations()
     {
-        createOutside();
-        createTheatre();
-        createPub();
-        createOffice();
-        createLab();
+        createEntrance();
+        createChamber();
+        createHallwayone();
+        createHallwaytwo();
+        createStudy();
+        createMagiclab();
+        createKitchen();
+        createDining();
+        createStairs();
+        createCentre();
 
-        currentLocation = outside;  // start game outside
+        currentLocation = entrance;  // start game outside
     }
     
     /**
      * Create the outside and link it to the
      * theatre, lab and pub
      */
-    private void createOutside()
+    private void createEntrance()
     {
-        outside = new Location("outside the main entrance of the university");
+        entrance = new Location("You stand in the entrance,The walls are cracked and the floor is withering away and the sound of battle rages outside the ironclad doors, something isn't right here. To the east there appeaes to be some kind of large chamber");
         
+        entrance.setExit("east", chamber);
     }
     
     /**
      * Create the pub and link it to the outside
      */
-    private void createPub()
+    private void createChamber()
     {
-        pub = new Location("in the campus pub");
+        chamber = new Location("The chamber is large and derelict, clearly someone hasn't been keeping the place clean. to the north and south are equally decrepit hallways, to the east is a large door with suspicious looking runes placed on it ");
         
-        pub.setExit("east", outside);
-        outside.setExit("west", pub);
+        chamber.setExit("north", hallwayone);
+        chamber.setExit("south", hallwaytwo);
+        if (dispell == 4)
+        {
+            chamber.setExit("east", stairs);
+        }
+        else
+        {
+            System.out.println("The magic on the door blocks you from opening it, you won't be getting in there without dispelling it first");
+        }
     }
     
     /**
      * Create the theatre linked to the outside
      */
-    private void createTheatre()
+    private void createHallwayone()
     {
-        theater = new Location("in a lecture theater");
+        hallwayone = new Location("in a lecture theater");
         
-        theater.setExit("west", outside);
-        outside.setExit("east", theater);
+        hallwayone.setExit("west", study);
+        hallwayone.setExit("east", magiclab);
     }
     
     /**
      * Create the office linked to the lab
      */
-    private void createOffice()
+    private void createHallwaytwo()
     {
-        office = new Location("in the computing admin office");
+        hallwaytwo = new Location("in the computing admin office");
         
+        hallwaytwo.setExit("west", kitchen);
+        hallwaytwo.setExit("east", dining);
     }
     
     /**
      * Create the lab and link it to the outside and office
      */
-    private void createLab()
+    private void createStudy()
     {
         // create the Locations
-        lab = new Location("in a computing lab");
+        study = new Location("in a computing lab");
         
-        lab.setExit("east", office);
-        office.setExit("west", lab);
+        study.setExit("east", hallwayone);
+    }
+    
+    /**
+     * Create the lab and link it to the outside and office
+     */
+    private void createMagiclab()
+    {
+        // create the Locations
+        magiclab = new Location("in a computing lab");
         
-        lab.setExit("north", outside);
-        outside.setExit("south", lab);
+        magiclab.setExit("west", hallwayone);
+    }
+    
+    /**
+     * Create the lab and link it to the outside and office
+     */
+    private void createKitchen()
+    {
+        // create the Locations
+        kitchen = new Location("in a computing lab");
+        
+        kitchen.setExit("east", hallwaytwo);
+    }
+    
+    /**
+     * Create the lab and link it to the outside and office
+     */
+    private void createDining()
+    {
+        // create the Locations
+        dining = new Location("in a computing lab");
+        
+        dining.setExit("west", hallwaytwo);
+    }
+    
+    /**
+     * Create the lab and link it to the outside and office
+     */
+    private void createStairs()
+    {
+        // create the Locations
+        stairs = new Location("in a computing lab");
+        
+        stairs.setExit("east", centre);
+    }
+    
+    /**
+     * Create the lab and link it to the outside and office
+     */
+    private void createCentre()
+    {
+        // create the Locations
+        centre = new Location("in a computing lab");
+        
     }
     
     public Location getCurrentLocation()
